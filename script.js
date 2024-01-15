cd721897f981180934bbe11a11b62fdb29c809db
Georgie <georgiehackett47@gmail.com>
2024-01-15 01:54:53 +0000

    Refactor code

--- script.js
+++ script.js
@@ -88,35 +88,51 @@ var upperCasedCharacters = [
   "Z",
 ];
 
+// Select textarea and button elements and assign to variables
+var textAreaEl = document.getElementById("password");
+var buttonEl = document.getElementById("generate");
+
+// Declare function to validate user has input either 'yes' or 'no'
 function checkInput(input) {
+  // Ensure function will validate for both lower and uppercase inputs
   var input = input.toLowerCase();
   if (input === "yes" || input === "no") {
     return true;
   } else {
-    alert("You must answer yes or no");
+    // Alert the user that their input has failed validation and exit function
+    alert("You must answer yes or no. Please try again.");
     return false;
   }
 }
 
-var textAreaEl = document.getElementById("password");
-var buttonEl = document.getElementById("generate");
+// Declare getRandom function
+function getRandom(arr) {
+  // Select a random number for the length of the arguement passed through the function
+  var getRandomEl = Math.floor(Math.random() * arr.length);
+  // Use the random number to select a random element from the array and assign to a variable
+  var randomEl = arr[getRandomEl];
+  return randomEl;
+}
 
+// Add click event listener to generate password button to begin getPasswordOptions function
 buttonEl.addEventListener("click", function getPasswordOptions() {
-  // console.log('hello');
+  
+  // Prompt user to input password criteria options
   var passwordLength = prompt(
     "How many characters would you like your password to contain? Choose between 8-128"
   );
+  // Validate passwordLength to ensure user input is a number and meets length criteria. Alert user and exit function if it does not
   var passwordLength = parseInt(passwordLength);
-  // console.log(passwordLength);
-  // console.log(typeof(passwordLength));
   if (isNaN(passwordLength) || 8 > passwordLength || 128 < passwordLength) {
     alert(
       "Please try again! Make sure your password contains between 8-128 characters."
     );
+    // If input criteria is met, continue to character type prompts
   } else {
     var containsLowercase = prompt(
       "Would you like your password to contain lowercase characters?"
     );
+    // Call checkInput function for containsLowercase to ensure user has input either 'yes' or 'no'
     if (checkInput(containsLowercase) === false) {
       return;
     } else {
@@ -137,122 +153,43 @@ buttonEl.addEventListener("click", function getPasswordOptions() {
           );
           if (checkInput(containsSpecial) === false) {
             return;
+            // Validate to ensure user has selected at least on charater type
           } else if (containsLowercase === 'no' && containsUppercase === 'no' && containsNumeric === 'no' && containsSpecial === 'no') {
             alert('You must select at least one option. Please try again.');
             return;
             } else {
 
+              // Declare empty array for charTypes
               var charTypes = [];
-              // console.log(charTypes);
+              
+              // Concatenate corresponding arrays for user's selected character types
               if (containsLowercase === 'yes') {
-                charTypes = charTypes.concat(lowerCasedCharacters);
-                // console.log(charTypes);
+                charTypes = charTypes.concat(lowerCasedCharacters);                
               }
               if (containsUppercase === 'yes') {
-                charTypes = charTypes.concat(upperCasedCharacters);
-                // console.log(charTypes);
+                charTypes = charTypes.concat(upperCasedCharacters);                
               }
               if (containsNumeric === 'yes') {
-                charTypes = charTypes.concat(numericCharacters);
-                // console.log(charTypes);
+                charTypes = charTypes.concat(numericCharacters);               
               }
               if (containsSpecial === 'yes') {
                 charTypes = charTypes.concat(specialCharacters);
-                // console.log(charTypes);
               }
-              
-              console.log(charTypes);
-              console.log(passwordLength);
 
+              // Declare empty userPassword variable
               var userPassword = '';
 
+              // Iterate through the user's chosen password length
               for (i = 0; i < passwordLength; i++) {
+                // Pass charTypes through getRandom function to select random element from user's chosen character types and add to userPassword
                 userPassword += getRandom(charTypes);
-                console.log(userPassword);
               }
 
-              textAreaEl.textContent = userPassword;
-              
+              // Set text content of textarea to userPassword result to display text on page
+              textAreaEl.textContent = userPassword;              
             }
           };
         }
       }
     }
   });
-
-function getRandom(arr) {
-  var getRandomEl = Math.floor(Math.random() * arr.length);
-  var randomEl = arr[getRandomEl];
-  console.log(randomEl);
-  return randomEl;
-}
-
-
-
-// console.log(passwordOptions);
-
-// Function to prompt user for password options
-// function getPasswordOptions() {
-//   var passwordLength = parseInt(
-//     prompt(
-//       "How many characters would you like your password to contain? Choose between 8-128"
-//     )
-//   );
-
-//   if (isNaN(passwordLength) || 8 > passwordLength || 128 < passwordLength) {
-//     alert(
-//       "Please try again! Make sure your password contains between 8-128 characters."
-//     );
-//   } else {
-//     var containsLowercase = confirm(
-//       "Would you like your password to contain lowercase characters?"
-//     );
-//     var containsUppercase = confirm(
-//       "Would you like your password to contain uppercase characters?"
-//     );
-//     var containsNumeric = confirm(
-//       "Would you like your password to contain numeric characters?"
-//     );
-//     var containsSpecial = confirm(
-//       "Would you like your password to contain special characters?"
-//     );
-//   }
-//   while (!containsLowercase && !containsUppercase && !containsNumeric && !containsSpecial) {
-//     alert("Please try again! Select at least one option.")
-//   }
-
-//   var userInput = {
-//     passwordLength: passwordLength,
-//     containsLowercase: containsLowercase,
-//     containsUppercase: containsUppercase,
-//     containsNumeric: containsNumeric,
-//     containsSpecial: containsSpecial,
-//   };
-
-//   return userInput;
-// }
-
-// getPasswordOptions();
-
-// // Function for getting a random element from an array
-// function getRandom(arr) {
-//   var randomElement = Math.floor(Math.random() * arr.length);
-//   return arr[randomElement];
-// }
-
-// // Function to generate password with user input
-// function generatePassword() {}
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
